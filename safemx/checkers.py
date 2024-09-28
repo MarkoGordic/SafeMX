21ddfca163e63968d6ae7ce6c67d317e9f114e97
Marko Gordic <mgordic22@gmail.com>
2024-09-28 15:41:32 +0200

    [v0.1.5] Handle mutiple SPF records

--- safemx/checkers.py
+++ safemx/checkers.py
@@ -4,42 +4,51 @@ from safemx.parsers import parse_spf_record, parse_dmarc_record, parse_dkim_reco
 
 def check_spf(domain, output_format='console'):
     spf_data = {}
+    spf_records = []
     try:
         answers = dns.resolver.resolve(domain, 'TXT')
         for rdata in answers:
             record = str(rdata).strip('"')
-
             if 'v=spf1' in record:
-                if output_format == 'console':
-                    print(f"{Fore.GREEN}[+] SPF record for {domain} found!{Style.RESET_ALL}")
-                    print(f"    spf: {record}\n")
+                spf_records.append(record)
 
-                spf_data = parse_spf_record(record, output_format)
-                return spf_data
+        if len(spf_records) > 1:
+            if output_format == 'console':
+                print(f"{Fore.RED}[!] Multiple SPF records found for {domain}. This is a misconfiguration!{Style.RESET_ALL}")
+                for spf in spf_records:
+                    print(f"    spf: {spf}\n")
+            else:
+                spf_data.update({"error": f"Multiple SPF records found for {domain}", "error_code": "MULTIPLE_SPF", "records": spf_records})
+            return spf_data
+
+        elif len(spf_records) == 1:
+            spf_record = spf_records[0]
+            if output_format == 'console':
+                print(f"{Fore.GREEN}[+] SPF record for {domain} found!{Style.RESET_ALL}\n    spf: {spf_record}\n")
+            return parse_spf_record(spf_record, output_format)
+
         if output_format == 'console':
             print(f"{Fore.RED}[!] No SPF record found for {domain}{Style.RESET_ALL}! Careful! Attackers can send emails on behalf of this domain.")
         else:
-            spf_data['error'] = f"No SPF record found for {domain}"
-            spf_data['error_code'] = "NO_SPF"
+            spf_data.update({"error": f"No SPF record found for {domain}", "error_code": "NO_SPF"})
+
     except dns.resolver.NoAnswer:
+        spf_data.update({"error": f"No SPF record found for {domain}", "error_code": "NO_SPF"})
         if output_format == 'console':
             print(f"{Fore.RED}[!] No SPF record found for {domain}{Style.RESET_ALL}")
-        else:
-            spf_data['error'] = f"No SPF record found for {domain}"
-            spf_data['error_code'] = "NO_SPF"
+
     except dns.resolver.NXDOMAIN:
+        spf_data.update({"error": f"Domain {domain} does not exist.", "error_code": "NXDOMAIN"})
         if output_format == 'console':
             print(f"{Fore.RED}[!] Domain {domain} does not exist.{Style.RESET_ALL}")
-        else:
-            spf_data['error'] = f"Domain {domain} does not exist."
-            spf_data['error_code'] = "NXDOMAIN"
+
     except Exception as e:
+        spf_data.update({"error": f"An error occurred while retrieving SPF: {e}", "error_code": "UNKNOWN"})
         if output_format == 'console':
             print(f"{Fore.RED}[!] An error occurred while retrieving SPF: {e}{Style.RESET_ALL}")
-        else:
-            spf_data['error'] = f"An error occurred while retrieving SPF: {e}"
-            spf_data['error_code'] = "UNKNOWN"
+
     return spf_data
+
 
 def check_dmarc(domain, output_format='console'):
     dmarc_data = {}
